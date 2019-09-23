910f1ae9091e4e73c8df24baab52091e3c5d1955
Jake Carlson <jcarlson93@ksu.edu>
2019-09-23 15:10:17 -0500

    Added the Drink base class

---
+++ Menu/Drinks/Drink.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Drinks
+{
+    /// <summary>
+    /// Enumerator for the drink flavors.
+    /// </summary>
+    public enum SodaSaurusFlavor
+    {
+        Cola,
+        Orange,
+        Vanilla,
+        Chocolate,
+        RootBeer,
+        Cherry,
+        Lime
+    }
+
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large
+    }
+
+    /// <summary>
+    /// Abstract class representing the drinks.
+    /// </summary>
+    public abstract class Drink
+    {
+        /// <summary>
+        /// Gets or sets the size.
+        /// </summary>
+        public abstract Size Size { get; set; }
+
+        /// <summary>
+        /// Tells whether the drink has ice or not.
+        /// </summary>
+        public bool Ice { get; protected set; } = true;
+
+        /// <summary>
+        /// Gets and sets the price
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Gets and sets the calories
+        /// </summary>
+        public uint Calories { get; set; }
+
+        /// <summary>
+        /// Gets the ingredient list.
+        /// </summary>
+        public abstract List<string> Ingredients { get; }
+
+        /// <summary>
+        /// Method to hold the ice for a drink.
+        /// </summary>
+        public void HoldIce()
+        {
+            this.Ice = false; 
+        }
+    }
+}
