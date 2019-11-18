c45b3fab25f0a2b2522b350f9861cfa6f769997f
Jake Carlson <jcarlson93@ksu.edu>
2019-11-18 13:13:10 -0600

    Website Milestone 1 Complete, Just need to add some more style

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
 
 namespace Website.Pages
 {
     public class MenuModel : PageModel
     {
+        // Private access variables
+        private Menu _menu = new Menu();
+
+        /// <summary>
+        /// Property to get the menu.
+        /// </summary>
+        public Menu Menu
+        {
+            get
+            {
+                return _menu;
+            }
+        }
+
         public void OnGet()
         {
 
